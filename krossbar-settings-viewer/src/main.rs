22c419d3818de58ffe1782114d0896018b8d2974
Alexander Smoktal <cosm.ua@gmail.com>
2024-06-04 16:14:51 +0300

    Implement viewer

--- krossbar-settings-viewer/src/main.rs
+++ krossbar-settings-viewer/src/main.rs
@@ -1,8 +1,9 @@
-use bson::Bson;
+use std::path::Path;
+
 use clap::{self, Parser};
 use colored::*;
-
-use krossbar_storage_common::{document::Document, get_storage_dir, queries::QueryRunner};
+use krossbar_settings_common::{settings::Settings, DEFAULT_SETTINGS_DIR};
+use serde_json::Value;
 
 /// Krossbar storage viewer
 #[derive(Parser, Debug)]
@@ -13,24 +14,18 @@ pub struct Args {
     pub target_service: String,
 }
 
-fn print_values(values: Vec<(String, Vec<u8>)>) {
-    for (name, value) in values.into_iter() {
-        let bson = bson::from_slice(&value).unwrap();
-        let document = bson::from_bson::<Document<Bson>>(bson).unwrap();
-
-        println!("{}: {}", name.bright_blue(), document.value())
+fn print_values(values: Vec<(String, Value)>) {
+    for (name, value) in values {
+        println!("{}: {}", name.bright_blue(), value)
     }
 }
 
 fn main() {
     let args = Args::parse();
+    let settings_path =
+        Path::new(DEFAULT_SETTINGS_DIR).join(format!("{}.json", &args.target_service));
 
-    let runner = QueryRunner::open(&format!(
-        "{}{}.db3",
-        get_storage_dir(),
-        &args.target_service
-    ))
-    .unwrap();
+    let mut settings = Settings::open(&settings_path).unwrap();
 
-    print_values(runner.list_values().unwrap());
+    print_values(settings.list_values().unwrap());
 }
